53cec32f42b51b1a7f11a3eb30ebc22a778213d1
Nyasper <herreragonzalo39@gmail.com>
2024-12-02 16:48:53 -0300

    models_updated

---
+++ src/app/creators/creator-detail/creator-detail.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creator-detail',
+  imports: [],
+  templateUrl: './creator-detail.component.html',
+  styleUrl: './creator-detail.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CreatorDetailComponent {
+
+}
---
+++ src/app/creators/creators.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creators',
+  imports: [],
+  templateUrl: './creators.component.html',
+  styleUrl: './creators.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CreatorsComponent {
+
+}
